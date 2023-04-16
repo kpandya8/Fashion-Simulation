2db90943dbaf129687f9a6adf19dbf2bf636847b
Krutik Pandya <kpandya8@asu.edu>
2023-04-16 18:40:26 +0000

    Update 10 files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client"
 import "./styles.css"
 import App from "./App"
+import Example from "./Homepage"
 
 createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById("rooted")).render(<Example />)
