d44a3d7e595df330c4a967fea567aa6f858ec92e
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2023-04-20 01:07:25 -0700

    potato

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import { createRoot } from "react-dom";
 import "./styles.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import LandingPage from "./LandingPage";
+import App from "./App";
 // import App from "./App";
 
 const root = document.getElementById("root");
 createRoot(root).render(
   <ChakraProvider>
-    <LandingPage />
-
-
-
-
-    
+    <App />
   </ChakraProvider>
 );
 
